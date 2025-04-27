1b4b3fed437cd40005332fbfd9d1c8a84864c2a0
Théo <thweis.ymc@gmail.com>
2025-04-28 01:57:23 +0200

    Initial Setup: File Tree, Dynamic Headers/Footers, and Story Assets Import
    Set up file tree structure, implemented dynamic header and footer (responsive to story progression). Imported folder containing base story, character backstories, and design assets.

---
+++ chapter1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php
+include("../global_includes/head.html");
+include("header.html"); ?>
+
+<body><img src="assets/NoxusInterArene.jpg" alt="Image de l'arène de Noxus"></body>
+
+<?php include("../global_includes/footer.html"); ?>
+
+</html>
